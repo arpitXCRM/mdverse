9929ee6d59c48a0a2f38969cae0c734f48cae748
arpitXCRM <arpit@promoxcrm.ai>
2025-02-09 21:49:25 +0530

    done login

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,17 +1,24 @@
-const express = require('express');
-const router = express.Router();
+const express = require("express");
 const passport = require("passport");
-const { facebookLogin, getFbLoginUrl } = require('../controllers/fbAuth');
-const { facebookCallback, getUser, logout } = require("../controllers/fbAuth-passport")
 
-// Routes for Facebook API Auth
-router.get('/facebook-api', getFbLoginUrl );
-router.get('/facebook-api/callback', facebookLogin);
+const { facebookLogin, getFbLoginUrl } = require("../controllers/fbAuth");
+const { facebookCallback, getUser, logout } = require("../controllers/fbAuth-passport");
+const { asyncHandler } = require("../middlewares/errorHandler");
+const { isAuthenticated } = require("../middlewares/authMiddleware");
 
-// Routes for Facebook auth by Passport library
+const router = express.Router();
+
+router.get("/facebook-api", asyncHandler(getFbLoginUrl));
+router.get("/facebook-api/callback", asyncHandler(facebookLogin));
+
 router.get("/facebook", passport.authenticate("facebook", { scope: ["email"] }));
-router.get("/facebook/callback", passport.authenticate("facebook", { failureRedirect: "/login" }), facebookCallback);
-router.get("/user", getUser);
-router.get("/logout", logout);
+router.get(
+  "/facebook/callback",
+  passport.authenticate("facebook", { failureRedirect: "/login" }),
+  asyncHandler(facebookCallback)
+);
+
+router.get("/user", isAuthenticated, asyncHandler(getUser));
+router.get("/logout", isAuthenticated, asyncHandler(logout));
 
 module.exports = router;
